c02923a0e548e6f4e9db421d47fb27f3c09ff019
EddyER01 <61298339+EddyER01@users.noreply.github.com>
2020-02-24 09:55:32 -0700

    Add files via upload

---
+++ Programa 7/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_7
+{
+    public partial class Form1 : Form
+    {
+        Temperatura Temperatura = new Temperatura();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"La Temperatura en Grados Farenheit es de {Temperatura.ConvertirAFarenheit()}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"La Temperatura dada en Grados Centigrados es de {Temperatura.GradosCentigrados}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Temperatura.GradosCentigrados = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
