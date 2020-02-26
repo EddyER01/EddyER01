572ae9e907b7425304112156467397e954081fdb
EddyER01 <61298339+EddyER01@users.noreply.github.com>
2020-02-26 10:46:06 -0600

    Add files via upload

---
+++ Programa 10/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tranajo_10
+{
+    class Restaurante
+    {
+        public int cantidad1 { get; set; }
+        public int cantidad2 { get; set; }
+        public int cantidad3 { get; set; }
+        public int cantidad4 { get; set; }
+        public int cantidad5 { get; set; }
+        public int cantidad6 { get; set; }
+        public double CalcularCuenta()
+        {
+            return 15 * cantidad1+18*cantidad2+20*cantidad3+8*cantidad4+5*cantidad5+6*cantidad6;
+        }
+    }
+}
